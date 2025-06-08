804e4e95dc96b5a7792f537f6a10a54daf402d9b
peaceRyun <dbsrn1110@gmail.com>
2025-06-08 17:57:07 +0900

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { IoIosAddCircle } from 'react-icons/io';
+
+const wrap = 'h-screen bg-[linear-gradient(to_right,#11998e,#38ef7d)] relative';
+
+const ItemWrap = 'w-full h-16 bg-gray-300';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div className={`${wrap}`}>
+                <h1 className='p-50 font-bold text-white text-shadow-2xs'>할일 추가</h1>
+                <div className='rounded-2xl w-[450px] h-[500px] shadow-2xl bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                    <div className='titleWrap flex justify-between items-center pt-8 px-8'>
+                        <div>date</div>
+                        <button className='w-20 h-20 p-0 transition-all duration-150 hover:text-yellow-500'>
+                            <IoIosAddCircle className='w-full h-full ' />
+                        </button>
+                    </div>
+                    <div className='inputWrap px-8 py-2'>
+                        <input
+                            type='text'
+                            value=''
+                            className='w-full border-b-2 text-xl'
+                            placeholder='할일을 입력하세요'
+                        />
+                    </div>
+                    <ul className='listWrap pt-5'>
+                        <li className={`${ItemWrap}`}>할일 1</li>
+                        <li className={`${ItemWrap}`}>할일 2</li>
+                        <li className={`${ItemWrap}`}>할일 3</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
